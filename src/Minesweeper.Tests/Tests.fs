88c57cda7961197922b712cccca7fd448f446f4a
SeungYongShim <syshim@me.com>
2022-06-12 19:17:12 +0900

    .

--- src/Minesweeper.Tests/Tests.fs
+++ src/Minesweeper.Tests/Tests.fs
@@ -21,3 +21,21 @@ let IsNumber() =
     let sut = Number 0
     let ret = sut |> char
     Assert.Equal('0', ret )
+
+[<Fact>]
+let IsFlag() =
+    let sut = Flag (Number 0)
+    let ret = sut |> char
+    Assert.Equal('A', ret )
+
+[<Fact>]
+let IsQuestion() =
+    let sut = Question (Number 0)
+    let ret = sut |> char
+    Assert.Equal('?', ret )
+
+[<Fact>]
+let IsQuestionWithRightClick() =
+    let sut = Covered (Number 0)
+    let ret = sut |> rclick |> rclick |> char
+    Assert.Equal('?', ret )
